eb43de2cf0409c0b373150297566c841810c630f
barox <joaogomesleite800@gmail.com>
2025-06-05 14:03:01 -0300

    first commit backend structure

---
+++ backend/src/main/java/com/gom/tasty_restaurants/TastyRestaurantsApplication.java
@@ -0,0 +1,13 @@
+package com.gom.tasty_restaurants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TastyRestaurantsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TastyRestaurantsApplication.class, args);
+	}
+
+}
