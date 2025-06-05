9b94f8c1d14ae85b60e39768adb32f9f882924b6
barox <joaogomesleite800@gmail.com>
2025-06-05 14:11:19 -0300

    feat: create class user

---
+++ backend/src/main/java/com/gom/tasty_restaurants/model/User.java
@@ -0,0 +1,34 @@
+package com.gom.tasty_restaurants.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnore
+    private List<Restaurant> restaurants = new ArrayList<>()  ;
+
+}
